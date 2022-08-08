81c8825d4cecc1cbbd23347207c049ce22a364cf
Victor Johan Palma Rodriguez <vicjohan23@gmail.com>
2022-08-08 01:39:42 -0400

    -Se creo el componente de acceso no permitido

---
+++ ManejadorCitasMedicas(MCM)/Utils/IJSExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace ManejadorCitasMedicas_MCM_.Utils
+{
+    public static class IJSExtensions
+    {
+        public static void Animar403(this IJSRuntime js)
+        {
+            js.InvokeVoidAsync("animar403");
+        }
+    }
+}
